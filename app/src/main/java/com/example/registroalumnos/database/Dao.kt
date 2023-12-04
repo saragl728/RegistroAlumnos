c7f3a211759cc8d38b211fff893024da1c016c00
Usuario <saragl728@gmail.com>
2023-12-04 09:33:23 +0100

    Añado cosas a la interfaz DAO

--- app/src/main/java/com/example/registroalumnos/database/Dao.kt
+++ app/src/main/java/com/example/registroalumnos/database/Dao.kt
@@ -1,4 +1,20 @@
 package com.example.registroalumnos.database
 
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
 interface Dao {
+    @Query("SELECT * FROM alumnos WHERE nombre like :nombre")
+    fun obtenerAlumnoPorNombre(nombre:String): Alumnos
+
+    @Insert
+    fun insertarAlumno(nombre:String, apellido:String, curso:String)
+
+    @Delete
+    fun borrarAlumno(nombre: String)
+
+    @Update
+    fun actualizarAlumno(nombre:String, curso:String)
 }
